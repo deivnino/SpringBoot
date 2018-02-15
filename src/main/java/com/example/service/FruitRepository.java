593da424305572659c60b26670d5a2511e50430d
deivnino <deivnino@gmail.com>
2018-02-15 15:59:40 -0500

    SpringBoot con Openshift

---
+++ src/main/java/com/example/service/FruitRepository.java
@@ -0,0 +1,10 @@
+package com.example.service;
+
+/**
+ * Created by dnino on 08/02/2018.
+ */
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FruitRepository extends CrudRepository<Fruit, Integer> {
+}
